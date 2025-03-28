15a1f6904b6629060d9d24a973a8970354dccf89
caond <caond@yahoo.com>
2025-03-28 10:55:23 +0800

    ass

--- R/synga-package.R
+++ R/synga-package.R
@@ -1,27 +1,47 @@
 #' synga: Example R Package
 #'
-#' This is an example package that demonstrates how to use a genetic algorithm to generate a synthetic dataset.
 #' library(synga)
+#'
 #' data(ed)
+#'
 #' data(data_type)
-#' #Step 1: Create the model
+#'
+#' # Step 1: Create the model
+#'
 #' ed_model <- create_model(ed, data_type)
-#' #Step 2: Generate synthetic data
+#'
+#' # Step 2: Generate synthetic data
+#'
 #' syn_cop <- generate_syn_copula(ed_model)
+#'
 #' syn_decomp <- generate_syn_decomp(ed_model)
+#'
 #' # Step 3: Generate ensemble model
+#'
 #' syn_en <-  ensemble_ga(ed_model,syn_cop,syn_decomp,100,'frobenius')
+#'
 #' syn_en_auc <-  ensemble_ga(ed_model,syn_cop,syn_decomp,100,'auc')
+#'
 #' # Step 4: Compare synthetic data with original data
+#'
 #' compare_dist(ed_model,syn_cop)
+#'
 #' compare_dist(ed_model,syn_decomp)
+#'
 #' compare_dist(ed_model,syn_en)
+#'
 #' compare_dist(ed_model,syn_en_auc)
+#'
 #' # Step 5: Compare synthetic data with original data
+#'
 #' benchmark_cor(ed_model,syn_cop)
+#'
 #' benchmark_cor(ed_model,syn_decomp)
+#'
 #' benchmark_cor(ed_model,syn_en)
+#'
 #' benchmark_cor(ed_model,syn_en_auc)
+#'
 #' @docType package
 #' @name synga
 NULL
